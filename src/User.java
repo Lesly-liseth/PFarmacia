45387f521788feb467997bd31b2b3f3726d131fb
Lesly <leslyherrera851@gmail.com>
2022-08-13 17:10:28 -0500

    creacion de la clase user
    se procedio a crear la clase user y a darle variables

---
+++ src/User.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class User extends JDialog {
+
+    public String NOMBRE;
+    public String EMAIL;
+    public String DIRECCION;
+    public String CELULAR;
+    public String CONTRASEÑA;
+
+}
--- src/login.java
+++ src/login.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
 
-public class login {
+public class login extends JDialog{
     private JTextField textField1;
     private JPasswordField passwordTF;
     private JButton cancelButton;
