f499d04bbe3fa1797f645241e60fa508ad5faf25
Lesly <leslyherrera851@gmail.com>
2022-08-12 21:44:30 -0500

    diseño de login
    se implemento el diseño del login

--- src/login.java
+++ src/login.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
 public class login {
+    private JTextField textField1;
+    private JPasswordField passwordTF;
+    private JButton cancelButton;
+    private JButton OKButton;
+    private JLabel emailTF;
+    private JPanel loginPanel;
+
 }
