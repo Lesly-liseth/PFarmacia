f4d8320b9c81d1173aad8c17b6ffa740694bf7f7
Mayra Ñaupari <mayra.naupari@epn.edu.ec>
2022-08-15 16:30:13 -0500

    Diseño
    Se le dio diseño a la parte de registro del producto
    agregando diferentes Label, espacios de texto y botones

--- src/productos_crud.java
+++ src/productos_crud.java
@@ -1,4 +1,14 @@
 import javax.swing.*;
 
 public class productos_crud {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton ACTUALIZARButton;
+    private JButton ELIMINARButton;
+    private JButton BUCARButton;
+    private JButton LIMPIARButton;
+    private JTextField textField5;
+    private JButton AGREGARButton;
 }
